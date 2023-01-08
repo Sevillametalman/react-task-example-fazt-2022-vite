4da3d06544e9e76918ce775d9bab34a806d6d077
Manuel Sevilla <59652842+Sevillametalman@users.noreply.github.com>
2023-01-08 05:02:34 -0400

    Csm

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,34 @@
+import { useState, useContext } from "react";
+import {TaskContext} from '../context/TaskContext';
+
+export function TaskForm() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const {createTask} = useContext(TaskContext)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTask({
+      title,
+      description
+    });
+  };
+  return (
+    <div className="max-w-md mx-auto">
+      <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+      <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
+      <input
+        placeholder="Escribe tu tarea"
+        className="bg-slate-300 p-3 w-full mb-2"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        placeholder="Escribe la descripcion"
+        className="bg-slate-300 p-3 w-full mb-2"
+        onChange={(e) => setDescription(e.target.value)}
+      ></textarea>
+      <button className="bg-indigo-500 px-3 py-1 text-white rounded">Guardar</button>
+    </form>
+    </div>
+  );
+}
